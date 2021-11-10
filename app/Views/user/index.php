3f678bba23c8b2b97ac9ad6cd8523ef236dca3b1
riftxdstudent <muhammad.rifqi2@student.umn.ac.id>
2021-11-10 16:18:04 +0700

    mengubah tampilan

--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -2,53 +2,23 @@
 
 <?= $this->section('content'); ?>
 
-<div class="container col-6">
-	<table>
-		<tbody>
-			<tr>
-				<td>ID</td>
-				<td> </td>
-				<td>:</td>
-				<td><?= $account['id']; ?></td>
-			</tr>
-			<tr>
-				<td>Email</td>
-				<td> </td>
-				<td>:</td>
-				<td><?= $account['email']; ?></td>
-			</tr>
-			<tr>
-				<td>Username</td>
-				<td> </td>
-				<td>:</td>
-				<td><?= $account['username']; ?></td>
-			</tr>
-			<tr>
-				<td>Nama</td>
-				<td> </td>
-				<td>:</td>
-				<td><?= $account['first_name'] . ' ' . $account['last_name']; ?></td>
-			</tr>
-			<tr>
-				<td>Tanggal Lahir</td>
-				<td> </td>
-				<td>:</td>
-				<td><?= $account['birth_date']; ?></td>
-			</tr>
-			<tr>
-				<td>Jenis Kelamin</td>
-				<td> </td>
-				<td>:</td>
-				<td><?= $account['gender'] == 1 ? 'Perempuan' : 'Laki-laki'; ?></td>
-			</tr>
-			<tr>
-				<td>Admin</td>
-				<td> </td>
-				<td>:</td>
-				<td><?= $account['is_admin'] == 1 ? 'Ya' : 'Tidak'; ?></td>
-			</tr>
-		</tbody>
-	</table>
+<div class="container col-6 text-center">
+	<div class="card mx-auto" style="width: 18rem;">
+		<img src="..." class="card-img-top" alt="...">
+		<div class="card-body">
+			<h5 class="card-title"><?= $account['first_name'] . ' ' . $account['last_name']; ?></h5>
+			<p class="card-text">
+				<?= $account['id']; ?> <br>
+				<?= $account['email']; ?> <br>
+				<?= $account['username']; ?>
+			</p>
+		</div>
+		<ul class="list-group list-group-flush">
+			<li class="list-group-item"><?= $account['birth_date']; ?></li>
+			<li class="list-group-item"><?= $account['gender'] == 1 ? 'Perempuan' : 'Laki-laki'; ?></li>
+			<li class="list-group-item"><?= $account['is_admin'] == 1 ? 'Admin' : 'Bukan Admin'; ?></li>
+		</ul>
+	</div>
 </div>
 
 <?= $this->endSection(); ?>
